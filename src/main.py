803cb1f14b8ad567a1b01cf020a12e04a6d297b3
by77er <by77er@gmail.com>
2023-04-21 03:47:50 -0400

    Add status (#4)
    💾

--- src/main.py
+++ src/main.py
@@ -7,10 +7,11 @@ bot = discord.Bot()
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game("memories on repeat 💾"), status=discord.Status.idle)
     print(f"{bot.user} is ready and online!")
 
 @bot.slash_command(name = "boop", description = "boop bitbot")
 async def hello(ctx):
     await ctx.respond("boop :3")
 
-bot.run(os.getenv('TOKEN'))
+bot.run(os.getenv("TOKEN"))
